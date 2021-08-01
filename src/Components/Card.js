1f2bd39b5758e8126f0e7039c28a9d3482920083
tru3-f8 <73862483+tru3-f8@users.noreply.github.com>
2021-08-01 15:41:12 -0700

    card-game

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,55 +3,45 @@ import axios from 'axios';
 
 function Card() {
   const [card, setCard] = useState([]);
-  const [cardsNumberSuits, setCardsNumberSuits] = useState();
 
-  console.log(cardsNumberSuits);
+
+  console.log(card)
+
 
   const deckOfCards = async () => {
     const url = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2';
 
     const response = await axios.get(url);
     const data = await response.data;
-    const result = data.cards.map((card) => card.image);
+    const result = data.cards.map((card) => ({ cardImage: card.image, cardValue: card.value }));
+
 
     return result;
   };
-
-  const cardsNumber = async () => {
-    const url = 'https://deckofcardsapi.com/api/deck/new/draw/?count=2';
-
-    const response = await axios.get(url);
-    const data = await response.data;
-    const numberSuits = data.cards.map((card) => card.code);
-
-    return numberSuits;
-  };
+                                                             
 
   const player = () => {
     deckOfCards().then((card) => {
       setCard(card);
     });
+                                                                        
 
-    cardsNumber().then((numberSuits) => {
-      numberSuits?.map((number) => setCardsNumberSuits(number));
-    });
-
-    console.log(card[0])
-    console.log(card[1])
-
-    if(card[0] > card[1]) {
-        console.log('you win')
+    if (card[0] > card[1]) {
+      console.log('you win');
     } else {
-        console.log('you lose')
+      console.log('you lose');
     }
-
-  
   };
+  
+
+
 
   return (
     <div>
-      <img src={card[0]} />
-      <img src={card[1]} />
+      <img src={card[0]?.cardImage} />
+      <h1>{card[0]?.cardValue}</h1>
+      <img src={card[1]?.cardImage} />
+      <h1>{card[1]?.cardValue}</h1>
       <button onClick={player}>Button</button>
     </div>
   );
